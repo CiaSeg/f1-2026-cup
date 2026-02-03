9a76f7ecd45b3a65f55fe54917d638ee0ad9326c
CiaSeg <luciasegrelles@gmail.com>
2026-02-03 12:20:55 +0100

    Arreglo crítico de rutas y duplicados

--- app.js
+++ app.js
@@ -15,168 +15,168 @@ class F1CupApp {
 
         // Datos estáticos (igual que en Streamlit)
         this.data = {
-            circuits: {
-                "GP Australia (06-08 Mar)": {
-                    nombre: "Albert Park", 
-                    bandera: "assets/circuitos/au.png", 
-                    mapa: "assets/circuitos/australia.png"
-                },
-                "GP China (13-15 Mar)": {
-                    nombre: "Shanghai", 
-                    bandera: "assets/circuitos/cn.png", 
-                    mapa: "assets/circuitos/china.png"
-                },
-                "GP Japón (27-29 Mar)": {
-                    nombre: "Suzuka", 
-                    bandera: "assets/circuitos/jp.png", 
-                    mapa: "assets/circuitos/japan.png"
-                },
-                "GP Bahrein (10-12 Apr)": {
-                    nombre: "Bahrain Int.", 
-                    bandera: "assets/circuitos/bh.png", 
-                    mapa: "assets/circuitos/bahrain.png"
-                },
-                "GP Arabia Saudí (17-19 Apr)": {
-                    nombre: "Jeddah", 
-                    bandera: "assets/circuitos/sa.png", 
-                    mapa: "assets/circuitos/saudi.png"
-                },
-                "GP Miami (01-03 May)": {
-                    nombre: "Miami", 
-                    bandera: "assets/circuitos/us.png", 
-                    mapa: "assets/circuitos/miami.png"
-                },
-                "GP Canadá (22-24 May)": {
-                    nombre: "Gilles-Villeneuve", 
-                    bandera: "assets/circuitos/ca.png", 
-                    mapa: "assets/circuitos/canada.png"
-                },
-                "GP Mónaco (05-07 Jun)": {
-                    nombre: "Monaco", 
-                    bandera: "assets/circuitos/mc.png", 
-                    mapa: "assets/circuitos/monaco.png"
-                },
-                "GP Barcelona (12-14 Jun)": {
-                    nombre: "Catalunya", 
-                    bandera: "assets/circuitos/es.png", 
-                    mapa: "assets/circuitos/barcelona.png"
-                },
-                "GP Austria (26-28 Jun)": {
-                    nombre: "Red Bull Ring", 
-                    bandera: "assets/circuitos/at.png", 
-                    mapa: "assets/circuitos/austria.png"
-                },
-                "GP Gran Bretaña (03-05 Jul)": {
-                    nombre: "Silverstone", 
-                    bandera: "assets/circuitos/gb.png", 
-                    mapa: "assets/circuitos/britain.png"
-                },
-                "GP Bélgica (17-19 Jul)": {
-                    nombre: "Spa", 
-                    bandera: "assets/circuitos/be.png", 
-                    mapa: "assets/circuitos/belgium.png"
-                },
-                "GP Hungría (24-26 Jul)": {
-                    nombre: "Hungaroring", 
-                    bandera: "assets/circuitos/hu.png", 
-                    mapa: "assets/circuitos/hungary.png"
-                },
-                "GP Países Bajos (21-23 Aug)": {
-                    nombre: "Zandvoort", 
-                    bandera: "assets/circuitos/nl.png", 
-                    mapa: "assets/circuitos/dutch.png"
-                },
-                "GP Italia (04-06 Sep)": {
-                    nombre: "Monza", 
-                    bandera: "assets/circuitos/it.png", 
-                    mapa: "assets/circuitos/italy.png"
-                },
-                "GP España - Madrid (11-13 Sep)": {
-                    nombre: "IFEMA Madrid", 
-                    bandera: "assets/circuitos/es.png", 
-                    mapa: "assets/circuitos/madrid.png"
-                },
-                "GP Azerbaiyán (24-26 Sep)": {
-                    nombre: "Baku", 
-                    bandera: "assets/circuitos/az.png", 
-                    mapa: "assets/circuitos/baku.png"
-                },
-                "GP Singapur (09-11 Oct)": {
-                    nombre: "Marina Bay", 
-                    bandera: "assets/circuitos/sg.png", 
-                    mapa: "assets/circuitos/singapore.png"
-                },
-                "GP Austin USA (23-25 Oct)": {
-                    nombre: "COTA", 
-                    bandera: "assets/circuitos/us.png", 
-                    mapa: "assets/circuitos/austin.png"
-                },
-                "GP México (30 Oct - 01 Nov)": {
-                    nombre: "Hermanos Rdz", 
-                    bandera: "assets/circuitos/mx.png", 
-                    mapa: "assets/circuitos/mexico.png"
-                },
-                "GP Brasil (06-08 Nov)": {
-                    nombre: "Interlagos", 
-                    bandera: "assets/circuitos/br.png", 
-                    mapa: "assets/circuitos/brazil.png"
-                },
-                "GP Las Vegas (19-21 Nov)": {
-                    nombre: "Las Vegas", 
-                    bandera: "assets/circuitos/us.png", 
-                    mapa: "assets/circuitos/vegas.png"
-                },
-                "GP Qatar (27-29 Nov)": {
-                    nombre: "Lusail", 
-                    bandera: "assets/circuitos/qa.png", 
-                    mapa: "assets/circuitos/qatar.png"
-                },
-                "GP Abu Dhabi (04-06 Dec)": {
-                    nombre: "Yas Marina", 
-                    bandera: "assets/circuitos/ae.png", 
-                    mapa: "assets/circuitos/abudhabi.png"
-                }
-            },
-            
-            drivers: {
-                "Albon": { foto: "assets/pilotos/albon.png", equipo: "Williams" },
-                "Alonso": { foto: "assets/pilotos/alonso.png", equipo: "Aston Martin" },
-                "Antonelli": { foto: "assets/pilotos/antonelli.png", equipo: "Mercedes" },
-                "Bearman": { foto: "assets/pilotos/bearman.png", equipo: "Haas" },
-                "Bortoleto": { foto: "assets/pilotos/bortoleto.png", equipo: "Audi" },
-                "Bottas": { foto: "assets/pilotos/bottas.png", equipo: "Cadillac" },
-                "Colapinto": { foto: "assets/pilotos/colapinto.png", equipo: "Alpine" },
-                "Gasly": { foto: "assets/pilotos/gasly.png", equipo: "Alpine" },
-                "Hadjar": { foto: "assets/pilotos/hadjar.png", equipo: "Red Bull" },
-                "Hamilton": { foto: "assets/pilotos/hamilton.png", equipo: "Ferrari" },
-                "Hulkenberg": { foto: "assets/pilotos/hulkenberg.png", equipo: "Audi" },
-                "Lawson": { foto: "assets/pilotos/lawson.png", equipo: "Racing Bulls" },
-                "Leclerc": { foto: "assets/pilotos/leclerc.png", equipo: "Ferrari" },
-                "Lindblad": { foto: "assets/pilotos/lindblad.png", equipo: "Racing Bulls" },
-                "Norris": { foto: "assets/pilotos/norris.png", equipo: "McLaren" },
-                "Ocon": { foto: "assets/pilotos/ocon.png", equipo: "Haas" },
-                "Perez": { foto: "assets/pilotos/perez.png", equipo: "Cadillac" },
-                "Piastri": { foto: "assets/pilotos/piastri.png", equipo: "McLaren" },
-                "Russell": { foto: "assets/pilotos/russell.png", equipo: "Mercedes" },
-                "Sainz": { foto: "assets/pilotos/sainz.png", equipo: "Williams" },
-                "Stroll": { foto: "assets/pilotos/stroll.png", equipo: "Aston Martin" },
-                "Verstappen": { foto: "assets/pilotos/verstappen.png", equipo: "Red Bull" }
-            },
-            
-            teams: {
-                "Alpine": "assets/equipos/alpine.png",
-                "Aston Martin": "assets/equipos/aston_martin.png",
-                "Audi": "assets/equipos/audi.png",
-                "Cadillac": "assets/equipos/cadillac.png",
-                "Ferrari": "assets/equipos/ferrari.png",
-                "Haas": "assets/equipos/haas.png",
-                "McLaren": "assets/equipos/mclaren.png",
-                "Mercedes": "assets/equipos/mercedes.png",
-                "Racing Bulls": "assets/equipos/rb.png",
-                "Red Bull": "assets/equipos/redbull.png",
-                "Williams": "assets/equipos/williams.png"
-            }
-        };
+    circuits: {
+        "GP Australia (06-08 Mar)": {
+            nombre: "Albert Park", 
+            bandera: "./assets/circuitos/au.png", 
+            mapa: "./assets/circuitos/australia.png"
+        },
+        "GP China (13-15 Mar)": {
+            nombre: "Shanghai", 
+            bandera: "./assets/circuitos/cn.png", 
+            mapa: "./assets/circuitos/china.png"
+        },
+        "GP Japón (27-29 Mar)": {
+            nombre: "Suzuka", 
+            bandera: "./assets/circuitos/jp.png", 
+            mapa: "./assets/circuitos/japan.png"
+        },
+        "GP Bahrein (10-12 Apr)": {
+            nombre: "Bahrain Int.", 
+            bandera: "./assets/circuitos/bh.png", 
+            mapa: "./assets/circuitos/bahrain.png"
+        },
+        "GP Arabia Saudí (17-19 Apr)": {
+            nombre: "Jeddah", 
+            bandera: "./assets/circuitos/sa.png", 
+            mapa: "./assets/circuitos/saudi.png"
+        },
+        "GP Miami (01-03 May)": {
+            nombre: "Miami", 
+            bandera: "./assets/circuitos/us.png", 
+            mapa: "./assets/circuitos/miami.png"
+        },
+        "GP Canadá (22-24 May)": {
+            nombre: "Gilles-Villeneuve", 
+            bandera: "./assets/circuitos/ca.png", 
+            mapa: "./assets/circuitos/canada.png"
+        },
+        "GP Mónaco (05-07 Jun)": {
+            nombre: "Monaco", 
+            bandera: "./assets/circuitos/mc.png", 
+            mapa: "./assets/circuitos/monaco.png"
+        },
+        "GP Barcelona (12-14 Jun)": {
+            nombre: "Catalunya", 
+            bandera: "./assets/circuitos/es.png", 
+            mapa: "./assets/circuitos/barcelona.png"
+        },
+        "GP Austria (26-28 Jun)": {
+            nombre: "Red Bull Ring", 
+            bandera: "./assets/circuitos/at.png", 
+            mapa: "./assets/circuitos/austria.png"
+        },
+        "GP Gran Bretaña (03-05 Jul)": {
+            nombre: "Silverstone", 
+            bandera: "./assets/circuitos/gb.png", 
+            mapa: "./assets/circuitos/britain.png"
+        },
+        "GP Bélgica (17-19 Jul)": {
+            nombre: "Spa", 
+            bandera: "./assets/circuitos/be.png", 
+            mapa: "./assets/circuitos/belgium.png"
+        },
+        "GP Hungría (24-26 Jul)": {
+            nombre: "Hungaroring", 
+            bandera: "./assets/circuitos/hu.png", 
+            mapa: "./assets/circuitos/hungary.png"
+        },
+        "GP Países Bajos (21-23 Aug)": {
+            nombre: "Zandvoort", 
+            bandera: "./assets/circuitos/nl.png", 
+            mapa: "./assets/circuitos/dutch.png"
+        },
+        "GP Italia (04-06 Sep)": {
+            nombre: "Monza", 
+            bandera: "./assets/circuitos/it.png", 
+            mapa: "./assets/circuitos/italy.png"
+        },
+        "GP España - Madrid (11-13 Sep)": {
+            nombre: "IFEMA Madrid", 
+            bandera: "./assets/circuitos/es.png", 
+            mapa: "./assets/circuitos/madrid.png"
+        },
+        "GP Azerbaiyán (24-26 Sep)": {
+            nombre: "Baku", 
+            bandera: "./assets/circuitos/az.png", 
+            mapa: "./assets/circuitos/baku.png"
+        },
+        "GP Singapur (09-11 Oct)": {
+            nombre: "Marina Bay", 
+            bandera: "./assets/circuitos/sg.png", 
+            mapa: "./assets/circuitos/singapore.png"
+        },
+        "GP Austin USA (23-25 Oct)": {
+            nombre: "COTA", 
+            bandera: "./assets/circuitos/us.png", 
+            mapa: "./assets/circuitos/austin.png"
+        },
+        "GP México (30 Oct - 01 Nov)": {
+            nombre: "Hermanos Rdz", 
+            bandera: "./assets/circuitos/mx.png", 
+            mapa: "./assets/circuitos/mexico.png"
+        },
+        "GP Brasil (06-08 Nov)": {
+            nombre: "Interlagos", 
+            bandera: "./assets/circuitos/br.png", 
+            mapa: "./assets/circuitos/brazil.png"
+        },
+        "GP Las Vegas (19-21 Nov)": {
+            nombre: "Las Vegas", 
+            bandera: "./assets/circuitos/us.png", 
+            mapa: "./assets/circuitos/vegas.png"
+        },
+        "GP Qatar (27-29 Nov)": {
+            nombre: "Lusail", 
+            bandera: "./assets/circuitos/qa.png", 
+            mapa: "./assets/circuitos/qatar.png"
+        },
+        "GP Abu Dhabi (04-06 Dec)": {
+            nombre: "Yas Marina", 
+            bandera: "./assets/circuitos/ae.png", 
+            mapa: "./assets/circuitos/abudhabi.png"
+        }
+    },
+    
+    drivers: {
+        "Albon": { foto: "./assets/pilotos/albon.png", equipo: "Williams" },
+        "Alonso": { foto: "./assets/pilotos/alonso.png", equipo: "Aston Martin" },
+        "Antonelli": { foto: "./assets/pilotos/antonelli.png", equipo: "Mercedes" },
+        "Bearman": { foto: "./assets/pilotos/bearman.png", equipo: "Haas" },
+        "Bortoleto": { foto: "./assets/pilotos/bortoleto.png", equipo: "Audi" },
+        "Bottas": { foto: "./assets/pilotos/bottas.png", equipo: "Cadillac" },
+        "Colapinto": { foto: "./assets/pilotos/colapinto.png", equipo: "Alpine" },
+        "Gasly": { foto: "./assets/pilotos/gasly.png", equipo: "Alpine" },
+        "Hadjar": { foto: "./assets/pilotos/hadjar.png", equipo: "Red Bull" },
+        "Hamilton": { foto: "./assets/pilotos/hamilton.png", equipo: "Ferrari" },
+        "Hulkenberg": { foto: "./assets/pilotos/hulkenberg.png", equipo: "Audi" },
+        "Lawson": { foto: "./assets/pilotos/lawson.png", equipo: "Racing Bulls" },
+        "Leclerc": { foto: "./assets/pilotos/leclerc.png", equipo: "Ferrari" },
+        "Lindblad": { foto: "./assets/pilotos/lindblad.png", equipo: "Racing Bulls" },
+        "Norris": { foto: "./assets/pilotos/norris.png", equipo: "McLaren" },
+        "Ocon": { foto: "./assets/pilotos/ocon.png", equipo: "Haas" },
+        "Perez": { foto: "./assets/pilotos/perez.png", equipo: "Cadillac" },
+        "Piastri": { foto: "./assets/pilotos/piastri.png", equipo: "McLaren" },
+        "Russell": { foto: "./assets/pilotos/russell.png", equipo: "Mercedes" },
+        "Sainz": { foto: "./assets/pilotos/sainz.png", equipo: "Williams" },
+        "Stroll": { foto: "./assets/pilotos/stroll.png", equipo: "Aston Martin" },
+        "Verstappen": { foto: "./assets/pilotos/verstappen.png", equipo: "Red Bull" }
+    },
+    
+    teams: {
+        "Alpine": "./assets/equipos/alpine.png",
+        "Aston Martin": "./assets/equipos/aston_martin.png",
+        "Audi": "./assets/equipos/audi.png",
+        "Cadillac": "./assets/equipos/cadillac.png",
+        "Ferrari": "./assets/equipos/ferrari.png",
+        "Haas": "./assets/equipos/haas.png",
+        "McLaren": "./assets/equipos/mclaren.png",
+        "Mercedes": "./assets/equipos/mercedes.png",
+        "Racing Bulls": "./assets/equipos/rb.png",
+        "Red Bull": "./assets/equipos/redbull.png",
+        "Williams": "./assets/equipos/williams.png"
+    }
+};
 
         // Datos dinámicos (desde Google Sheets y local)
         this.sheetsData = {
